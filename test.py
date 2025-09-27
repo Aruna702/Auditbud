8138d159cb67a48e3341b750cbb81d964cece2de
Aruna702 <arunabv070802@gmail.com>
2025-09-27 16:00:16 +0530

    test.py

--- test.py
+++ test.py
@@ -71,7 +71,7 @@ div.stButton > button {
 col1, col2 = st.columns([1, 6])
 
 with col1:
-    st.image("/mnt/data/f940fbd0-bc8e-4369-addc-a7b077cc5eed.png", width=80)
+    st.image("image.png", width=80)
 
 with col2:
     st.markdown("""
